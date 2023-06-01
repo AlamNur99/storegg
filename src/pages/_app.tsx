55426bebe61637932ce6925d2ecfb33af72b82ce
AlamNur99 <muh.alamnur.haris99@gmail.com>
2023-06-01 18:25:22 +0800

    slicing detail page UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
+import type { AppProps } from 'next/app'
 import '@/styles/utilities.css'
 import '@/styles/homepage.css'
-import type { AppProps } from 'next/app'
+import '@/styles/detail.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
