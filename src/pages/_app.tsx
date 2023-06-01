370195886305d8278e8bb9f60a12ff220b333d34
AlamNur99 <muh.alamnur.haris99@gmail.com>
2023-06-01 21:30:33 +0800

    convert complete checkout page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '@/styles/utilities.css'
 import '@/styles/homepage.css'
 import '@/styles/detail.css'
 import '@/styles/checkout.css'
+import '@/styles/complete-checkout.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
