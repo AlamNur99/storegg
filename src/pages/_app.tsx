071674e7ecb6e8dc0233b305361657a39bb8760b
AlamNur99 <muh.alamnur.haris99@gmail.com>
2023-06-01 21:23:04 +0800

    convert checkout page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import '@/styles/utilities.css'
 import '@/styles/homepage.css'
 import '@/styles/detail.css'
+import '@/styles/checkout.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
