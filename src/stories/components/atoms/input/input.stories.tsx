408938e1edc6bcaaea4d8054632d27510c9b23bf
AlamNur99 <muh.alamnur.haris99@gmail.com>
2023-06-07 16:47:12 +0800

    global style in storybook

--- src/stories/components/atoms/input/input.stories.tsx
+++ src/stories/components/atoms/input/input.stories.tsx
@@ -1,4 +1,5 @@
-import { Meta } from '@storybook/react';
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
 import Input, { InputProps } from '../../../../../components/atoms/Input';
 
 export default {
@@ -6,9 +7,10 @@ export default {
   component: Input,
 } as Meta;
 
-const Template = (args: InputProps) => <Input {...args} />;
+const Template: Story<InputProps> = (args) => <Input {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
   label: 'Nama Lengkap',
+  placeHolder: 'Masukkan Nama Lengkap',
 };
